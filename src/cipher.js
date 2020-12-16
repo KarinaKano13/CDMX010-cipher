bd55e744c19c4c627c7351cab13f1f0ced971f7b
KarinaKano13 <karinakanoarte@gmail.com>
2020-12-16 13:26:40 -0600

    Cifra y esta en modulos

--- src/cipher.js
+++ src/cipher.js
@@ -1,41 +1,32 @@
-function cifrar(){
-  const text = document.getElementById("mensaje").value;  // Lo que me trae a JavaScript el valor de desplazamiento:
-  const mayusText = text.toUpperCase();// lo convierte a Mayusculas
-  const offset = document.getElementById("offset").value;  //Lo que me trae al JavaScript el valor del mensaje:
-  const offsetNumber = parseInt(offset); // Convierto el desplazamiento a valor Numérico.
-  const final = [];
-  const espacio = " "
+const cipher = {
 
-  for (let i = 0; i<mayusText.length; i++){// For loop que me toma los valores de mi texto y les aplica el charCodeAt:
-    let asciiValue = mayusText[i].charCodeAt(); //aqui le asigno el valor de i a asciiValue
-      if (asciiValue >= 65 && asciiValue <= 90) {
-          //console.log(asciiValue)
-        let ubication= (asciiValue-65+offsetNumber)%26+65;
-         // console.log(ubication);
+  encode(mayusText,offsetNumber) {
+  let final=[]
+  
+    for (let i = 0; i < mayusText.length; i++) { // For loop que me toma los valores de mi texto y les aplica el charCodeAt:
+      let asciiValue2 = mayusText.charCodeAt(i);  // 'hola'.charCodeAt(3) = 65
+
+      if (asciiValue2 >= 65 && asciiValue2 <= 90) {
+         
+        let ubication= (asciiValue2-65+offsetNumber)%26+65;
+         
         let mensajeCifrado = String.fromCharCode(ubication);
+        
           final.push(mensajeCifrado);
-        //let mensajeCifrado2 = mensajeCifrado;
-            /* for (let i = 0; i <= mensajeCifrado.length; i++) {
-            let resultado = mensajeCifrado[i].String();*/
-        //document.getElementById("x").innerHTML = mensajeCifrado.toString();//este me lo devuelve pero la ultima letra
-          document.getElementById("x").innerHTML = final.join(""); // me sale el mensaje en consola pero en el elemento p me sale undefined
-        //return mensajeCifrado    
-         //let resultado = mensajeCifrado;   
-         // console.log(mensajeCifrado);
-    }else{
-      final.push(espacio);  
-      document.getElementById("x").innerHTML = final.join("");
-          //return mensajeInvalido;
-          //onsole.log(mensajeInvalido);
-    } 
-  } 
-}
+          
+       }  
+      else {
+        final.push(mayusText[i]);  // Hola mundo!
+            
+      } 
+     }
 
- function descifrar() {
-  let text2 = document.getElementById("mensaje2").value;  // Lo que me trae a JavaScript el valor de desplazamiento:
-  let mayusText2 = text2.toUpperCase();// lo convierte a Mayusculas
-  let offset2 = document.getElementById("offset2").value;  //Lo que me trae al JavaScript el valor del mensaje:
-  let offsetNumber2 = parseInt(-(offset2)); // Convierto el desplazamiento a valor Numérico.
+    return final.join("");
+  }
+}
+/*
+ function decode() {
+  
   let final2 = [];
   let espacio2 = " "
 
@@ -48,10 +39,10 @@ function cifrar(){
         let mensajeDescifrado2 = String.fromCharCode(ubication2);
           final2.push(mensajeDescifrado2);
         //let mensajeCifrado2 = mensajeCifrado;
-            /* for (let i = 0; i <= mensajeCifrado.length; i++) {
-            let resultado = mensajeCifrado[i].String();*/
+            for (let i = 0; i <= mensajeCifrado.length; i++) {
+            let resultado = mensajeCifrado[i].String();
         //document.getElementById("x").innerHTML = mensajeCifrado.toString();//este me lo devuelve pero la ultima letra
-          document.getElementById("x2").innerHTML = final2.join(""); // me sale el mensaje en consola pero en el elemento p me sale undefined
+         document.getElementById("x2").innerHTML = final2.join(""); // me sale el mensaje en consola pero en el elemento p me sale undefined
         //return mensajeCifrado    
          //let resultado = mensajeCifrado;   
          console.log(mensajeDescifrado2);
@@ -63,9 +54,12 @@ function cifrar(){
     } 
   }    
  }
+*/
+ export default cipher;
+ /*
+ const cipher = {
+  cifrado: document.getElementById("myBotonId").addEventListener("click",encode),
+  descifrar:document.getElementById("myBotonId2").addEventListener("click",decode)
+ }
 
-const cipher = {
- cifrado: document.getElementById("myBotonId").addEventListener("click",cifrar),
- descifrar:document.getElementById("myBotonId2").addEventListener("click",descifrar)
-}
-export default cipher
+*/
--- src/index.js
+++ src/index.js
@@ -1,14 +1,59 @@
 import cipher from './cipher.js';
 
-  //cipher.cifrado = document.getElementById("myBotonId").addEventListener("click",cifrar),
-  //document.descifrar = getElementById("myBotonId2").addEventListener("click",descifrar)
+const boton =document.getElementById("myBotonId");
 
+  boton.addEventListener('click', function (e) {
+    e.preventDefault();
+    const text = document.getElementById("mensaje").value;  // Lo que me trae a JavaScript el valor de desplazamiento:
+    // lo convierte a Mayusculas 
+    const mayusText = text.toUpperCase();
+    const offset = document.getElementById("offset").value;  //Lo que me trae al JavaScript el valor del mensaje:
+    // Convierto el desplazamiento a valor Numérico.
+    const offsetNumber = parseInt(offset);
+    const parrafo = document.getElementById("resultado");
+    
 
+     let final2 = cipher.encode(mayusText,offsetNumber)
+    parrafo.innerHTML=final2;
+    console.log(final2)
+   
+    }
+  )
+/*
+  document.getElementById("miBotonId2").addEventListener('click',function descifrado() {
+    const text2 = document.getElementById("mensaje2").value;  // Lo que me trae a JavaScript el valor de desplazamiento:
+    const mayusText2 = text2.toUpperCase();// lo convierte a Mayusculas
+    const offset2 = document.getElementById("offset2").value;  //Lo que me trae al JavaScript el valor del mensaje:
+    const offsetNumber2 = parseInt(-(offset2)); // Convierto el desplazamiento a valor Numérico.
 
+      document.getElementById("resultado2").innerHTML = cipher.decode(mayusText2,offsetNumber2);
+  }
+)*/
+/*
+  const encode= {
+    
+    text: document.getElementById("mensaje").value,  // Lo que me trae a JavaScript el valor de desplazamiento:
+    const: mayusText = text.toUpperCase(),// lo convierte a Mayusculas
+    const:offset = document.getElementById("offset").value,  //Lo que me trae al JavaScript el valor del mensaje:
+    const:offsetNumber = parseInt(offset), // Convierto el desplazamiento a valor Numérico.
+    
+    cifrado: document.getElementById("myBotonId").addEventListener("click",cipher.encode(offsetNumber,mayusText)),
+   
+   }
 
+   const decode={
 
+    text2: document.getElementById("mensaje2").value,  // Lo que me trae a JavaScript el valor de desplazamiento:
+    mayusText2: text2.toUpperCase(),// lo convierte a Mayusculas
+    offset2: document.getElementById("offset2").value,  //Lo que me trae al JavaScript el valor del mensaje:
+    offsetNumber2: parseInt(-(offset2)), // Convierto el desplazamiento a valor Numérico.
 
+    descifrado: document.getElementById("myBotonId2").addEventListener("click",cipher.decode(offsetNumber2,mayusText2)),
 
+   }
+
+   import cipher from './cipher.js';
+*/
 
 
 
