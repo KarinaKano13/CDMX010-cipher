8d27a9abf284b5490c3aafd372c3dace541bc74a
KarinaKano13 <karinakanoarte@gmail.com>
2020-12-22 11:27:36 -0600

    Cifrado y decifrado

--- src/cipher.js
+++ src/cipher.js
@@ -1,83 +1,83 @@
 const cipher = {
 
-  encode(mayusText,offsetNumber) {
+  encode(mayusText, offsetNumber) {
 
-  let final=[]
+    let final = []
 
-  if(offsetNumber == null){
-    throw new TypeError ("not valid");
-  }
-    else if(offsetNumber==[]){
+    if (offsetNumber == null) {
+      throw new TypeError("not valid");
+    }
+    else if (offsetNumber == []) {
 
-    throw new TypeError("not valid");
-  }
-    else if(offsetNumber==0){
+      throw new TypeError("not valid");
+    }
+    else if (offsetNumber == 0) {
 
-    throw new TypeError("not valid")
-  }
-  
+      throw new TypeError("not valid")
+    }
+
     for (let i = 0; i < mayusText.length; i++) { // For loop que me toma los valores de mi texto y les aplica el charCodeAt:
       let asciiValue2 = mayusText.charCodeAt(i);  // 'hola'.charCodeAt(3) = 65
 
       if (asciiValue2 >= 65 && asciiValue2 <= 90) {
-         
-        let ubication= ((asciiValue2-65+offsetNumber)%26)+65;
-         
+
+        let ubication = ((asciiValue2 - 65 + offsetNumber) % 26) + 65;
+
         let mensajeCifrado = String.fromCharCode(ubication);
-        
-          final.push(mensajeCifrado);
-          
-      }  
+
+        final.push(mensajeCifrado);
+
+      }
       else {
 
         final.push(mayusText[i]);  // Hola mundo!
-            
-      } 
+
+      }
 
     }
 
     return final.join("");
   },
 
-  decode(mayusText2,offsetNumber2) {
+  decode(mayusText2, offsetNumber2) {
 
-    let final2=[]
+    let final2 = []
 
-    if(offsetNumber2 == null){
-
-      throw new TypeError ("not valid");
-    }
-      else if(offsetNumber2==[]){
+    if (offsetNumber2 == null) {
 
       throw new TypeError("not valid");
     }
-    else if(offsetNumber2==0){
+    else if (offsetNumber2 == []) {
+
+      throw new TypeError("not valid");
+    }
+    else if (offsetNumber2 == 0) {
 
       throw new TypeError("not valid")
     }
-        
-      for (let i = 0; i < mayusText2.length; i++) { // For loop que me toma los valores de mi texto y les aplica el charCodeAt:
-        
-        let asciiValueD = mayusText2.charCodeAt(i);  // 'hola'.charCodeAt(3) = 65
-  
-        if (asciiValueD >= 65 && asciiValueD <= 90) {
-           
-          let ubicationD= ((asciiValueD - 65 + offsetNumber2 + 26) % 26 + 65 );
-           
-          let mensajeDescifrado2 = String.fromCharCode(ubicationD);
 
-            final2.push(mensajeDescifrado2);
-            
-        }
-        
-        else {
+    for (let i = 0; i < mayusText2.length; i++) { // For loop que me toma los valores de mi texto y les aplica el charCodeAt:
 
-          final2.push(mayusText2[i]);  // Hola mundo!
-              
-        } 
+      let asciiValueD = mayusText2.charCodeAt(i);  // 'hola'.charCodeAt(3) = 65
+
+      if (asciiValueD >= 65 && asciiValueD <= 90) {
+
+        let ubicationD = ((asciiValueD - 65 + offsetNumber2 + 26) % 26 + 65);
+
+        let mensajeDescifrado2 = String.fromCharCode(ubicationD);
+
+        final2.push(mensajeDescifrado2);
+
       }
-  
-      return final2.join("");
+
+      else {
+
+        final2.push(mayusText2[i]);  // Hola mundo!
+
+      }
+    }
+
+    return final2.join("");
   }
 
 }
